86a42934dc0fa218f76b23bb0c37e0ff4c6f37d4
Ky Nguyen <kynguyen@VN-LW11480-M.local>
2019-03-04 09:45:21 +0700

    Init project

--- GrabRewards/ViewController.swift
+++ GrabRewards/ViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: knStaticListController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
     }
 
 
