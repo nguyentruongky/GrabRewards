dbc53489e703d7d70e2b5bbe497fd5b7788b5729
Ky Nguyen <nguyentruongky33@gmail.com>
2019-03-04 09:38:50 +0700

    Initial Commit

---
+++ GrabRewards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrabRewards
+//
+//  Created by Ky Nguyen on 3/4/19.
+//  Copyright © 2019 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
